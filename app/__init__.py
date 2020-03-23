4e6fe91716548b6fa3431d5e148f6eb28cdefdb8
mr1451 <mr1451@georgetown.edu>
2020-03-23 17:53:08 -0400

    Reorganize repo

---
+++ app/__init__.py
@@ -0,0 +1,5 @@
+# nothing to see here
+
+# helps other commands recognize that there are files within this dir which we care to import from
+
+# the dir where __init__.py exists, treat it as a module
---
+++ conftest.py
@@ -0,0 +1,5 @@
+# helps pytest recognize the structure fo your repo 
+
+# helps pytest require the structure
+
+# [ut thios in the root dir of your repo
